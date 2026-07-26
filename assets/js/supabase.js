fe90aa29f935008dfbe14d34872bd8ae73d61d8d
Keymerv <usextools@gmail.com>
2026-07-26 08:02:06 +0300

    Update supabase.js

--- assets/js/supabase.js
+++ assets/js/supabase.js
@@ -1,1 +1,9 @@
+// استيراد مكتبة Supabase من الـ CDN الرسمي
+import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm'
 
+// بيانات مشروعك الفعلي
+const SUPABASE_URL = 'https://jpsybjkydhvoslrnwkoj.supabase.co'
+const SUPABASE_ANON_KEY = 'sb_publishable_vRZkK2oAh-1SjWzSX0BcLQ_1TNHbKqO'
+
+// إنشاء عميل Supabase الرئيسي وتصديره
+export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY)
